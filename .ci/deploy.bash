9261bbd3706c1b8a40e584a352a6c29dc3408425
Sam Murray <sammurray@microsoft.com>
2023-02-25 16:11:10 +0000

    hello

--- .ci/deploy.bash
+++ .ci/deploy.bash
@@ -5,4 +5,4 @@ set -e
 docker build -t brawler-server -f .ci/godot-server-env.dockerfile .
 docker save brawler-server | gzip > brawler-server.tar.gz
 scp brawler-server.tar.gz azureuser@20.81.125.206:/home/azureuser
-ssh azureuser@20.81.125.206 "docker stop brawler || docker load -i brawler-server.tar.gz && docker run --name brawler --rm -d -it -p 4433:4433 brawler-server"
+ssh azureuser@20.81.125.206 "docker stop brawler || docker load -i brawler-server.tar.gz && docker run --name brawler --rm -d -it -p 4433:4433/tcp -p 4433:4433/udp brawler-server"
